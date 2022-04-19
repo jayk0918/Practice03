bce84c50dc870d4a93c31214310c812e1255228d
Mocking_jayk0918 <jesper2able@gmail.com>
2022-04-19 16:19:28 +0900

    1.0(java_practice_Q20solved 22.04.19 16:19)

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -10,43 +10,39 @@ public class Ex20 {
 		// 질문한 숫자가 결정된 숫자보다 높으면 "더 낮게", 낮으면 "더 높게"
 		// 정답을 맞춘경우 "맞았습니다"
 		// 게임반복을 위해 y/n이라 묻고 n인 경우 종료, y면 게임 다시 시작
-		
 		Scanner sc = new Scanner(System.in);
-		
 		while(true) {
-		System.out.println("답안입력");
-		int answer = sc.nextInt();
-		
-		System.out.println("======================");
-		System.out.println("   [숫자맞추기게임 시작]   ");
-		System.out.println("======================");
-		
-		System.out.print(">>");
-		while(true) {
-			int guess = sc.nextInt();
+			System.out.println("답안입력");
+			int answer = sc.nextInt();
+			int guess;
 			
-			if(guess>answer) {
-				System.out.println("더 낮게");
-			}else if(guess<answer) {
-				System.out.println("더 높게");
-			}else{
-				System.out.println("맞았습니다.");
-				System.out.print("게임을 종료하시겠습니까?(y/n) >>");
-				String retry = sc.nextLine();
-				sc.nextLine();
-				if(retry == "y"){
-					System.out.println("======================");
-					System.out.println("   [숫자맞추기게임 종료]   ");
-					System.out.println("======================");
+			System.out.println("================");
+			System.out.println("[숫자맞추기게임 시작]");
+			System.out.println("================");
+			
+			while(true) {
+				System.out.print(">>");
+				guess = sc.nextInt();
+				if(answer == guess) {
+					System.out.println("맞았습니다.");
 					break;
-				}else if(retry == "n"){
-					
+				}else if(answer>guess) {
+					System.out.println("더 높게");
+				}else {
+					System.out.println("더 낮게");
 				}
+			} // guess while문 종료
+			
+			System.out.println("게임을 종료하시겠습니까?(y/n) >>");
+			char retry = sc.next().charAt(0);
+			if(retry == 'y'){
+				System.out.println("================");
+				System.out.println("[숫자맞추기게임 종료]");
+				System.out.println("================");
+				break;
 			}
-		}
+			
+		} // main-game while문 종료
 		sc.close();
-		}
-	}
-	
-}
-
+	}// main method 종료
+}// class body 종료
